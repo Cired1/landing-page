0354f8f74a5a9adabd90c77058b3c2fe28091f74
Cired1 <edabundis@gmail.com>
2022-02-24 22:45:37 -0600

    Add about page

--- src/routes/AboutPage/AboutPage.js
+++ src/routes/AboutPage/AboutPage.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
-import styles from "./AboutPage.module.css"
-import Construction from "../../components/Construction/Construction";
+import styles from "./AboutPage.module.css";
+import missionImage from "../../assets/mission.jpg";
 
 const AboutPage = () => {
 
@@ -11,7 +11,26 @@ const AboutPage = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <Construction />
+                <h2 className={styles.title}>
+                    Meet the minds behind Example Net
+                </h2>
+                <p className={styles.desc}>
+                    Learn what defines us as a team, as a company, and check open positions if you want to be one of us!
+                </p>
+                <div className={styles.mission}>
+                    <div className={styles.missionLeft}>
+                        <h3 className={styles.missionTitle}>Our mission</h3>
+                        <p className={styles.missionDesc}>
+                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                            Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. 
+                        </p>
+                    </div>
+                    <img 
+                        className={styles.image} 
+                        src={missionImage} 
+                        alt="mission" 
+                    />
+                </div>
             </div>
         </section>
     )
