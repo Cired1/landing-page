2f308e438037a17a0d0dd2ce8fa78d76e76588d7
Cired1 <edabundis@gmail.com>
2022-02-24 23:00:49 -0600

    Change service page layout

--- src/routes/ServicesPage/ServicesPage.js
+++ src/routes/ServicesPage/ServicesPage.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import styles from "./ServicesPage.module.css";
-import { Services } from "../../components"
 
 const ServicesPage = () => {
 
@@ -13,7 +12,24 @@ const ServicesPage = () => {
             <div className={styles.content}>
                 <h2 className={styles.title}>More about our services</h2>
                 <h3 className={styles.subtitle}>Digital Products</h3>
-                <Services />
+                <section className={styles.grid}>
+                    <div className={styles.item}>
+                        <h4>Service 1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h4>Service 2</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h4>Service 3</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div className={styles.item}>
+                        <h4>Service 4</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </section>
             </div>
         </section>
     )
