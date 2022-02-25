3b4694a5be8d9e7058ed5b7a1f058f16033f8d8f
Cired1 <edabundis@gmail.com>
2022-02-24 23:30:00 -0600

    Add contact page

--- src/routes/ContactPage/ContactPage.js
+++ src/routes/ContactPage/ContactPage.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react"
-import Construction from "../../components/Construction/Construction";
 import styles from "./ContactPage.module.css"
 
 const ContactPage = () => {
@@ -8,10 +7,23 @@ const ContactPage = () => {
         document.title = "Example Net | Contact"
     }, []);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <Construction />
+                <div className={styles.left}>
+                    <h2 className={styles.title}>Need help?</h2>
+                    <p className={styles.desc}>Fill up the form and our team will get back to you within 24 hours</p>
+                </div>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <textarea rows="8" cols="50" placeholder="Message"></textarea>
+                    <button className={styles.btn}>Send Message</button>
+                </form>
             </div>
         </section>
     )
